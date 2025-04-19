18fd6880f17ce3d07555702c78ce0feb126c27e2
amantyagi9999 <amantyagi431@gmail.com>
2025-04-19 21:33:35 +0530

    Initial commit

---
+++ airbnbapp/src/main/java/com/project/airbnbapp/AirbnbappApplication.java
@@ -0,0 +1,13 @@
+package com.project.airbnbapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirbnbappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirbnbappApplication.class, args);
+	}
+
+}
---
+++ airbnbapp/src/test/java/com/project/airbnbapp/AirbnbappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.airbnbapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirbnbappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
